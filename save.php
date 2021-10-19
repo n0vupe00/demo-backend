135a4fcd8ccddeafe0e89d518fb8d03e6fc333ee
Pekka Vuokila <n0vupe00@students.oamk.fi>
2021-10-19 07:48:39 +0300

    save2

--- save.php
+++ save.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin:' . $_SERVER['HTTP_ORIGIN']);
 header('Access-Control-Allow-Credentials:true');
-header('Access-Control-Allow-Methods: GET,POST,PUT; DELETE;OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Max-Age: 3600');
 header('Content-Type: application/json');
